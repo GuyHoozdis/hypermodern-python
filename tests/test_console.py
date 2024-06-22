fac3efde79f6d5627a31b3c8b3406861b3b30356
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-06-22 10:54:06 -0500

    Move unit test into appropriate module

--- tests/test_console.py
+++ tests/test_console.py
@@ -15,22 +15,16 @@ def mock_wikipedia_random_page(mocker):
     return mocker.patch("hypermodern_python.wikipedia.random_page")
 
 
+# TODO: This is an integration test, not a unit test.
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
+# TODO: This is an integration test, not a unit test.
 def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert "Guy Hoozdis" in result.output
-
-
-# TODO: This test should be moved into test_wikipedia.py and modified appropriately for the new context.
-def test_main_fails_on_request_error(runner, mock_requests_get):
-    mock_requests_get.side_effect = Exception("Boom")
-    result = runner.invoke(console.main)
-    assert result.exit_code == 1
-    assert "Boom" == str(result.exception)
 
 
 # TODO: This is an integration test, not a unit test.
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,4 +1,14 @@
+import click
+import pytest
+import requests
+
 from hypermodern_python import wikipedia
+
+
+def test_random_page_raises_click_exception_on_requests_error(mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
 
 
 def test_random_page_uses_given_language(mock_requests_get):
