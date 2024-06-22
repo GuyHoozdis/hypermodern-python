c8ca05401ad92399549e6f75b6333c74aaefeac1
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-06-21 19:20:30 -0500

    Add more robust tests and improve test coverage config

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,26 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+@pytest.fixture
+def mock_reqeusts_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Guy Hoozdis",
+        "extract": "Who is this guy?",
+    }
+    return mock
+
+
+def test_main_succeeds(runner, mock_reqeusts_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_reqeusts_get):
+    result = runner.invoke(console.main)
+    assert "Guy Hoozdis" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_reqeusts_get):
+    result = runner.invoke(console.main)
+    assert mock_reqeusts_get.called
