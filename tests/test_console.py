a3ee02bd3edaf9292a55b7b146b5b77b2f07d256
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-06-22 09:01:23 -0500

    Improve CLI handling fatal exception

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 import click.testing
 import pytest
+import requests
 
 from hypermodern_python import console, wikipedia
 
@@ -10,7 +11,7 @@ def runner():
 
 
 @pytest.fixture
-def mock_reqeusts_get(mocker):
+def mock_requests_get(mocker):
     mock = mocker.patch("requests.get")
     mock.return_value.__enter__.return_value.json.return_value = {
         "title": "Guy Hoozdis",
@@ -19,23 +20,31 @@ def mock_reqeusts_get(mocker):
     return mock
 
 
-def test_main_succeeds(runner, mock_reqeusts_get):
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_main_prints_title(runner, mock_reqeusts_get):
+def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert "Guy Hoozdis" in result.output
 
 
-def test_main_uses_en_wikipedia_org(runner, mock_reqeusts_get):
+# TODO: This test should be moved into test_wikipedia.py and modified appropriately for the new context.
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
     _ = runner.invoke(console.main)
-    mock_reqeusts_get.assert_called_once_with(wikipedia.API_URL)
+    mock_requests_get.assert_called_once_with(wikipedia.API_URL)
 
 
-def test_main_fails_on_request_error(runner, mock_reqeusts_get):
-    mock_reqeusts_get.side_effect = Exception("Boom")
+# TODO: This test should be moved into test_wikipedia.py and modified appropriately for the new context.
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
     assert "Boom" == str(result.exception)
+
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
