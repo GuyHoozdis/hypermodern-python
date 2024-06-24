5c29825b5cb132fe2e0909e64ba074fca1e664d4
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-06-24 11:53:51 -0500

    Add timeout to expected parameters on mock call
    One of the flake8 plugins identified that a call to requests without a
    timeout is a potential issue.  Adding that parameter caused the
    `assert_called_once_with` test to fail because it didn't account for the
    new kwargs of `timeout`.

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import click
 import pytest
 import requests
@@ -15,4 +17,4 @@ def test_random_page_uses_given_language(mock_requests_get):
     language = "de"
     wikipedia.random_page(language=language)
     url = wikipedia.get_api_url_for(language=language)
-    mock_requests_get.assert_called_once_with(url)
+    mock_requests_get.assert_called_once_with(url, timeout=mock.ANY)
