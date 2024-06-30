87b8de84d191b7dd2d2c7e9c6db050b45e000855
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-06-30 13:50:45 -0500

    Gracefully handle schema validation errors

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -25,3 +25,9 @@ def test_random_page_uses_given_language(mock_requests_get: mock.Mock) -> None:
 def test_random_page_returns_page(mock_requests_get: mock.Mock) -> None:
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
+
+
+def test_random_page_handles_validation_errors(mock_requests_get: mock.Mock) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = None
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
