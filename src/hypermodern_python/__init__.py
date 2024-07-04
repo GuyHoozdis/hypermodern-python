35fa9a8cc71e81c1726c5cfd83d5cd4f11330788
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-04 10:07:59 -0500

    I've been forgetting to bump the version

--- src/hypermodern_python/__init__.py
+++ src/hypermodern_python/__init__.py
@@ -3,4 +3,4 @@
 # TODO: Change to single source version strategy.
 #   https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-package-version
 #   https://docs.python.org/3.11/library/importlib.metadata.html#module-importlib.metadata
-__version__ = "0.3.0"
+__version__ = "0.5.0"
