bc6ce4ff29ec59fe44cba64368843d3656d4a1f0
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-05 18:25:38 -0500

    Single source package version

--- src/hypermodern_guyhoozdis/__init__.py
+++ src/hypermodern_guyhoozdis/__init__.py
@@ -1,6 +1,9 @@
 """The Hypermodern Python Project."""
 
-# TODO: Change to single source version strategy.
-#   https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-package-version
-#   https://docs.python.org/3.11/library/importlib.metadata.html#module-importlib.metadata
-__version__ = "0.6.6"
+from importlib.metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
