063bbb9ab24c1b21f2abbf68f9df2b2a8cfd5841
Guy Hoozdis <GuyHoozdis@users.noreply.github.com>
2024-07-06 18:10:41 -0500

    Feature/4 managing dependencies in nox (#19)
    * Bump version number
    
    * Improvements to the test workflows
    
    * xdoctests session done
    
    * docs session done
    
    * mypy session done
    
    * typeguard session done
    
    * black session done
    
    * Shorten install parameters
    
    * Coverage session invokes tests directly
    
    * lint session done
    
    * Bump version
    
    * Remove old PR template
    
    * New PR template

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,40 @@
+## WHY ARE WE DOING THIS
+
+(Your content here)
+
+#### Related Tickets and PRs
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+<!-- !!!! Leave a link to the task for this PR and any documentation. !!!!! -->
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+<!-- -->
+<!-- Replace the content below with a *link* to your task. -->
+
+_There is not a ticket for this [task][task]._
+
+[task]: http://gph.is/2kkVn66
+
+
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+<!-- !!!!   Developer Help: GitHub Flavored Markdown (GFM) Resources   !!!! -->
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+
+[gfm-syntax]: https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+[gfm-links]: https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#links
+[autolinking]: https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/autolinked-references-and-urls#issues-and-pull-requests
+[gfm-general]: https://support.github.com/
+
+
+## Test Plan
+<!-- Recommended section - delete if not used -->
+
+Steps to verify change:
+
+1. Step 1
+1. Step 2
+
+
+#### Expected Result
+
+- Before this change it zigs.
+- After this change it zags.
+
--- noxfile.py
+++ noxfile.py
@@ -18,18 +18,11 @@ SOURCE_CODE_TARGETS = ["src/", "tests/", "./noxfile.py", "docs/conf.py"]
 package = "hypermodern_guyhoozdis"
 
 
-# TODO: Return to reimplement this.  For now, just make note the signature.
-# def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
-#     pass
-
-
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     args = session.posargs or ["-m", "not e2e"]
-    session.run("poetry", "install", "--only", "main", external=True)
-
-    session.install("pytest", "pytest-mock", "typeguard")
+    session.run("poetry", "install", "--with=mypy", external=True)
     session.run("pytest", "--typeguard-packages={package}", *args)
 
 
@@ -37,46 +30,26 @@ def typeguard(session: Session) -> None:
 def mypy(session: Session) -> None:
     """Static type checking using mypy."""
     args = session.posargs or SOURCE_CODE_TARGETS
-    session.install("mypy")
-
-    # XXX: Doing this to workaround the install_with_constraints issue.
-    session.run("poetry", "install", "--no-root", external=True)
-    session.install("types-requests")
-
+    session.run("poetry", "install", "--with=mypy", external=True)
     session.run("mypy", *args)
 
 
-# TODO: Do I need to keep this if I am using flake8-black?
-# A: Maybe.  When flake8-black runs it may report an issue without enough detail to fix it.  In that
-#    case, I've been running `nox -s black` to resolve the issue.  However, there seems to be a
-#    difference in the configuration of the two.  I haven't looked into that yet.
+# When flake8-black runs it may report an issue without enough detail to fix it.  In that
+# case, I've been running `nox -s black` to resolve the issue.
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def black(session: Session) -> None:
     """Run black code formatter."""
     args = session.posargs or SOURCE_CODE_TARGETS
-    session.install("black")
+    session.run("poetry", "install", "--only=black", external=True)
     session.run("black", *args)
 
 
 # TODO: Replace flake8-import-order with flake8-isort or pre-commit.
-# TODO: Does flake8 behave differently under different python versions?
-# - Should this be running for different python versions or can it be just one?
-# - Can I dynamically select the python version being used by the dev and just use that instead of hardcode?
-# @nox.session(python=PYTHON_VERSIONS)
 @nox.session(python="3.11")
 def lint(session: Session) -> None:
     """Lint using flake8 and plugins."""
     args = session.posargs or SOURCE_CODE_TARGETS
-    session.install(
-        "flake8",
-        "flake8-annotations",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-bugbear",
-        "flake8-docstrings",
-        "flake8-import-order",
-        "darglint",
-    )
+    session.run("poetry", "install", "--no-root", "--only=lint", external=True)
     session.run("flake8", *args)
 
 
@@ -93,22 +66,21 @@ def tests(session: Session) -> None:
 def xdoctests(session: Session) -> None:
     """Run examples with xdoctest."""
     args = session.posargs or ["all"]
-    session.run("poetry", "install", "--only", "main", external=True)
-    session.install("xdoctest")
+    session.run("poetry", "install", external=True)
     session.run("python", "-m", "xdoctest", package, *args)
 
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def docs(session: Session) -> None:
     """Build the documentation."""
-    session.run("poetry", "install", "--with", "main,docs", external=True)
+    session.run("poetry", "install", "--without=dev", "--with=docs", external=True)
     session.run("sphinx-build", "docs", "docs/_build")
 
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def coverage(session: Session) -> None:
     """Upload coverage data."""
-    session.install("coverage[toml]", "codecov")
+    tests(session)
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
 
